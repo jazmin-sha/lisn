e8f4fd9607391fa6f52d8231c4703fdea44e4a1a
jazmin-sha <jazminshanu642k@gmail.com>
2025-02-12 22:01:26 +0530

    Fix default path issue

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,6 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent },
-  { path: '**', redirectTo: 'home' },
+  { path: '', component: HomeComponent },
+  { path: '**', redirectTo: '' },
 ];
